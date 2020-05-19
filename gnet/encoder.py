e88d1c9800ddf4776c2b601a1cc6dd83286ff14d
Noah Trenaman <noahtren@gmail.com>
2020-05-18 22:25:47 -0400

    simple multi-head attention works

--- gnet/encoder.py
+++ gnet/encoder.py
@@ -57,20 +57,18 @@ class GlobalAttention(tf.keras.layers.Layer):
 
     # apply masking for if num_nodes < max_nodes
     mask = tf.sequence_mask(num_nodes, maxlen=x.shape[1])[:, tf.newaxis]
-    grid_max_idxs = tf.math.reduce_max(
-      tf.cast(tf.where(
+    # a bunch of gross syntax that creates a broadcastable 2D mask:
+    grid_max_idxs = tf.tile(tf.expand_dims(tf.math.reduce_max(
+      tf.cast(tf.reshape(tf.where(
         tf.ones((x.shape[1], x.shape[1]))
-      ), tf.int32), axis=-1)
-    mask = tf.where(grid_max_idxs < num_nodes,
-      tf.ones((x.shape[1], x.shape[1]), dtype=tf.bool),
-      tf.zeros((x.shape[1], x.shape[1]), dtype=tf.bool)
-    )
-    mask = tf.ones((num_nodes,))
+      ), (x.shape[1], x.shape[1], 2)), tf.int32), axis=-1), 0),
+      [num_nodes.shape[0], 1, 1])
+    mask = grid_max_idxs < num_nodes[:, tf.newaxis, tf.newaxis]
     Es = [tf.where(mask, e, tf.ones_like(e) * -1e9) for e in Es]
 
     # calculate alignment scores and aggregate contexts
     scores = [tf.nn.softmax(e) for e in Es]
-    contexts = [tf.matmul(score, q) for score, q in zip(scores, Qs)]
+    contexts = [tf.matmul(score, v) for score, v in zip(scores, Vs)]
     contexts = tf.concat(contexts, axis=-1)
 
     # produce new features from full context
