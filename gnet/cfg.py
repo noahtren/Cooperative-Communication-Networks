8a9f45f04615b9e76a4b24f0853ac4b9108c69db
Noah Trenaman <noahtren@gmail.com>
2020-05-27 16:52:47 -0400

    more experiments with graph only

--- gnet/cfg.py
+++ gnet/cfg.py
@@ -1,5 +1,5 @@
-VISION = True
-JUST_VISION = True
+VISION = False
+JUST_VISION = False
 CFG = {
   # dataset
   'max_nodes': 5,
@@ -8,7 +8,7 @@ CFG = {
   'language_spec': 'arithmetic',
   'min_num_values': 2,
   'max_num_values': 3,
-  'num_samples': 1_000 if JUST_VISION else 15_000,
+  'num_samples': 2_500 if JUST_VISION else 15_000,
 
   # graph models
   'hidden_size': 512,
@@ -21,7 +21,7 @@ CFG = {
 
   # vision models
   'VISION': VISION,
-  'vision_model': 'conv',
+  'vision_model': 'conv', # could also be CPPN
   'DISC_MODEL' : 'ResNet50V2',
   'y_dim': 128,
   'x_dim': 128,
@@ -29,9 +29,9 @@ CFG = {
   'vision_hidden_size': 512,
   'cppn_loc_embed_dim': 32,
   'gen_Z_embed_dim': 512,
-  'c_out': 3,
-  'generator_lr': 0.0001,
-  'discriminator_lr': 0.0001,
+  'c_out': 1,
+  'generator_lr': 0.00005,
+  'discriminator_lr': 0.00005,
   'NUM_SYMBOLS': 256, # for toy problem
   'use_perceptual_loss': False,
   'use_distance_loss': False,
@@ -39,19 +39,19 @@ CFG = {
   'use_custom_disc': True,
 
   # training
-  'batch_size': 32,
+  'batch_size': 64,
   'epochs': 3_000,
   'mse_loss_only': False,
-  'label_smoothing': 0.0 if VISION else 0.0,
+  'label_smoothing': 0.0,
 
   # checkpointing and saving (set to NOLOG to prevent logging)
-  'run_name': 'slow_normal_experiment_256symbols_contd2',
+  'run_name': 'graph1',
   # 'run_name': 'graph_pretrain',
   # 'load_name': 'graph_pretrain',
-  'load_name': 'slow_normal_experiment_256symbols_contd',
+  'load_vision_name': 'slow_normal_experiment_256symbols_bw',
+  'load_graph_name': None
 }
 RESULTS = {}
 
-if CFG['vision_model']:
+if CFG['vision_model'] == 'conv':
   assert CFG['x_dim'] == CFG['y_dim']
-  
