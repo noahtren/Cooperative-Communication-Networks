a5f52f32d9b33e7ab488c28658ee95ab0e5acb59
Noah Trenaman <noahtren@gmail.com>
2020-05-31 19:10:54 -0400

    animate interpolation between tree glyphs

--- gnet/animate.py
+++ gnet/animate.py
@@ -11,13 +11,17 @@ import numpy as np
 import imageio
 from tqdm import tqdm
 
+from encoder import Encoder
+from decoder import GraphDecoder
 from vision import Generator, Decoder
 from train_vision import make_data
 from cfg import CFG
 from ml_utils import load_ckpts
+from graph_data import TensorGraph, get_dataset, label_data
+from graph_match import minimum_loss_permutation
 
 
-def make_sliding_window_data(points:List[int], steps_between:int):
+def make_sliding_window_symbols(points:List[int], steps_between:int):
   """Given a list of points to interpolate between and the number
   of steps to use in interpolation, return a 2D tensor of inputs
   that follow the interpolation order -- in order to produce a
@@ -38,6 +42,67 @@ def make_sliding_window_data(points:List[int], steps_between:int):
   return data
 
 
+def get_tree_results(steps_between:int, models):
+  # generate a list of distinct expression trees
+  adj_lists = [
+    [[], [], [0, 1]],
+    [[], [], [0, 1]]
+  ]
+  value_tokens_lists = [
+    ['a', 'a', '-'],
+    ['a', 'a', '+']
+  ]
+  order_lists = [
+    [-1, -1, -1],
+    [-1, -1, -1]
+  ]
+  # convert to TensorGraphs
+  tgs = []
+  for i in range(len(adj_lists)):
+    tree_args = TensorGraph.parse_tree(CFG['language_spec'], adj_lists[i],
+      value_tokens_lists[i], order_lists[i], CFG['max_nodes'])
+    tg = TensorGraph.tree_to_instance(*tree_args, CFG['max_nodes'], CFG['language_spec'])
+    tgs.append(tg)
+
+  adj, node_features, node_feature_specs, num_nodes = TensorGraph.instances_to_tensors(tgs)
+
+  _ = models['encoder'][0]({'node_features': node_features, 'num_nodes': num_nodes, 'adj': adj})
+  Z = models['encoder'][0]({'node_features': node_features, 'num_nodes': num_nodes, 'adj': adj})
+  x = Z
+
+  states = []
+  for i in range(x.shape[0] - 1):
+    diff = x[i + 1] - x[i]
+    for j in range(steps_between):
+      ratio = j / steps_between
+      state = x[i] + diff * ratio
+      states.append(state)
+  states = tf.stack(states, axis=0)
+  # generate images
+
+  _ = models['generator'][0](Z)
+  imgs = models['generator'][0](Z)
+
+  # evaluate performance of models
+  nf_labels, adj_labels = label_data(node_features, adj)
+
+  _ = models['discriminator'][0](imgs)
+  Z = models['discriminator'][0](imgs)
+
+  _ = models['decoder'][0](x)
+  adj_pred, nf_pred = models['decoder'][0](Z)
+
+  batch_loss, acc = minimum_loss_permutation(
+    adj_labels,
+    nf_labels,
+    adj_pred,
+    nf_pred
+  )
+  imgs = models['generator'][0](states)
+  return states, imgs, acc
+
+
+
 def circle_crop(img):
   y_dim = img.shape[0]
   x_dim = img.shape[1]
@@ -52,17 +117,41 @@ def circle_crop(img):
 
 
 if __name__ == "__main__":
-  data = make_sliding_window_data([3,8,9,3], 30)
+  CFG['num_samples'] = 1
+  _, _, node_feature_specs, _, \
+  _, _ = get_dataset(**CFG, test=False)
+  CFG['node_feature_specs'] = node_feature_specs
+  encoder = Encoder(**CFG)
   generator = Generator()
-  generator(tf.expand_dims(data[0], 0))
-  models = {'generator': [generator, 0]}
-  load_ckpts(models, CFG['load_name'], 'latest')
-  writer = imageio.get_writer(f"gallery/{CFG['load_name']}_animation.mp4", format='FFMPEG', fps=20)
-  for i, val in tqdm(enumerate(data)):
-    val = tf.expand_dims(val, 0)
-    img = generator(val)
-    img = tf.squeeze((img + 1) / 2)
-    img = tf.cast(img * 255, tf.uint8)
-    img = circle_crop(img)
-    writer.append_data(np.array(img))
-  writer.close()
+  discriminator = Decoder()
+  decoder = GraphDecoder(**CFG)
+  if CFG['JUST_VISION']:
+    data = make_sliding_window_symbols([3,8,9,3], 30)
+    generator(tf.expand_dims(data[0], 0))
+    models = {'generator': [generator, None]}
+    load_ckpts(models, CFG['load_vision_name'], 'latest')
+    writer = imageio.get_writer(f"gallery/{CFG['load_name']}_animation.mp4", format='FFMPEG', fps=20)
+    for i, val in tqdm(enumerate(data)):
+      val = tf.expand_dims(val, 0)
+      img = generator(val)
+      img = tf.squeeze((img + 1) / 2)
+      img = tf.cast(img * 255, tf.uint8)
+      img = circle_crop(img)
+      writer.append_data(np.array(img))
+    writer.close()
+  else:
+    models = {
+      'encoder': [encoder, None],
+      'generator': [generator, None],
+      'discriminator': [discriminator, None],
+      'decoder': [decoder, None]
+    }
+    load_ckpts(models, CFG['load_graph_name'], 'best')
+    states, imgs, acc = get_tree_results(30, models)
+    writer = imageio.get_writer(f"gallery/{CFG['load_graph_name']}_animation.mp4", format='FFMPEG', fps=20)
+    for i, img in tqdm(enumerate(imgs)):
+      img = tf.squeeze((img + 1) / 2)
+      img = tf.cast(img * 255, tf.uint8)
+      img = circle_crop(img)
+      writer.append_data(np.array(img))
+    writer.close()
