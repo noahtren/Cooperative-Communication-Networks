e466babc09932eda054eed6cadb869474a027bb5
Noah Trenaman <noahtren@gmail.com>
2020-06-02 16:37:25 -0400

    config in JSON instead of Python module

--- gnet/cfg.py
+++ gnet/cfg.py
@@ -1,64 +1,28 @@
-# optionally read config from disk, maybe to restore a particular checkpoint
+"""Read config from file when running session. This can be set up for training
+multiple different models with different hyperparameter settings.
+"""
 
-VISION = False
-JUST_VISION = False
-CFG = {
-  # dataset
-  'max_nodes': 5,
-  # 5! = 120
-  # 6! = 720
-  'language_spec': 'arithmetic',
-  'min_num_values': 2,
-  'max_num_values': 3,
-  'num_samples': 2_500 if JUST_VISION else 15_000,
+import os
+import json
 
-  # graph models
-  'graph_hidden_size': 512, # also used in discriminator module
-  'encoder_attention_layers': 4,
-  'decoder_attention_layers': 4,
-  'num_heads': 6,
-  # the inline if statement is a helpful pattern
-  'initial_lr': 0.00001 if VISION else 0.0001,
-  'use_exponential_rate_scheduler': True,
+code_path = os.path.dirname(os.path.abspath(__file__))
 
-  # vision models
-  'JUST_VISION': JUST_VISION,
-  'VISION': VISION,
-  'vision_model': 'conv', # could also be CPPN
-  'DISC_MODEL' : 'ResNet50V2',
-  'y_dim': 128,
-  'x_dim': 128,
-  'R': 7,
-  'vision_hidden_size': 512,
-  'minimum_filters': 0, # set to 0 for no minimum
-  'Z_embed_num': 5,
-  'cppn_loc_embed_dim': 32,
-  'gen_Z_embed_dim': 512,
-  'c_out': 1,
-  'generator_lr': 0.0001,
-  'discriminator_lr': 0.0001,
-  'NUM_SYMBOLS': 256, # for toy problem
-  'use_perceptual_loss': False,
-  'use_distance_loss': False,
-  'pretrained_disc': 'ResNet50V2',
-  'use_custom_disc': True,
 
-  # training
-  'batch_size': 24,
-  'epochs': 3_000,
-  'mse_loss_only': False,
-  'label_smoothing': 0.0,
+def validate_cfg(CFG):
+  if CFG['vision_model'] == 'conv':
+    assert CFG['x_dim'] == CFG['y_dim']
 
-  # checkpointing and saving (set to NOLOG to prevent logging)
-  'run_name': 'new_run_graph',
-  # 'load_graph_name': None,
-  'load_graph_name': None,
-  # 'load_vision_name': 'full_may27_4',
-  'load_vision_name': None,
-  'aws_filepath': '/gnet',
-  'aws_bucket': 'gestalt-graph',
-}
-RESULTS = {}
 
-if CFG['vision_model'] == 'conv':
-  assert CFG['x_dim'] == CFG['y_dim']
+def parse_cfg(CFG):
+  if CFG['USE_S3']:
+    CFG['save_checkpoint_every'] = CFG['s3_save_checkpoint_every']
+    CFG['root_filepath'] = CFG['s3_root_filepath']
+  else:
+    CFG['save_checkpoint_every'] = CFG['default_save_checkpoint_every']
+    CFG['root_filepath'] = CFG['default_root_filepath']
+  return CFG
+
+
+CFG = json.load(open(os.path.join(code_path, 'config.json'), 'r'))
+CFG = parse_cfg(CFG)
+print(f"Config: {json.dumps(CFG, indent=4)}")
