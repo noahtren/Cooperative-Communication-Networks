e8eba30991774687b17c321a4fdc3a81408068b6
Noah Trenaman <noahtren@gmail.com>
2020-06-03 19:23:02 -0400

    improve management of config between local and cloud

--- gnet/cfg.py
+++ gnet/cfg.py
@@ -7,22 +7,38 @@ import json
 
 code_path = os.path.dirname(os.path.abspath(__file__))
 
+access_path = os.path.join(code_path, "gestalt-graph-59b01bb414f3.json")
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = access_path
+
 
 def validate_cfg(CFG):
   if CFG['vision_model'] == 'conv':
     assert CFG['x_dim'] == CFG['y_dim']
 
 
+def populate_cfg(CFG):
+  CFG['TPU'] = False
+  if 'IS_TPU' in os.environ:
+    if os.environ['IS_TPU'] == 'y':
+      CFG['TPU'] = True
+  return CFG
+
+
 def parse_cfg(CFG):
-  if CFG['USE_S3']:
-    CFG['save_checkpoint_every'] = CFG['s3_save_checkpoint_every']
-    CFG['root_filepath'] = CFG['s3_root_filepath']
-  else:
-    CFG['save_checkpoint_every'] = CFG['default_save_checkpoint_every']
-    CFG['root_filepath'] = CFG['default_root_filepath']
+  CFG['save_checkpoint_every'] = CFG['cloud_save_checkpoint_every']
+  CFG['root_filepath'] = CFG['gs_root_filepath']
+  CFG['VISION'] = CFG['JUST_VISION'] or CFG['FULL']
+  # if CFG['USE_S3']:
+  #   CFG['save_checkpoint_every'] = CFG['cloud_save_checkpoint_every']
+  #   CFG['root_filepath'] = CFG['s3_root_filepath']
+  # else:
+  #   CFG['save_checkpoint_every'] = CFG['default_save_checkpoint_every']
+  #   CFG['root_filepath'] = CFG['default_root_filepath']
   return CFG
 
 
 CFG = json.load(open(os.path.join(code_path, 'config.json'), 'r'))
+CFG = populate_cfg(CFG)
 CFG = parse_cfg(CFG)
+validate_cfg(CFG)
 print(f"Config: {json.dumps(CFG, indent=4)}")
